8ec56134cbd5be0d454fded8d6fc54b29e26e9c3
Adrian Hill <adrian.hill@mailbox.org>
2020-11-05 17:34:23 +0100

    Test all preconditioners
    Cholesky currently fails at when `iter<2`

--- test/TVRegDiff_large_test.jl
+++ test/TVRegDiff_large_test.jl
@@ -3,18 +3,22 @@ using Random
 using Statistics
 using Test
 
-function _eval_noisy(f, u, preconditioner; iter=1, rng_seed=0)
+using Plots
+using LaTeXStrings
+
+function _eval_large_noisy(f, u; iter=1, preconditioner="amg_rs", rng_seed=0)
     n = 1000
     x = range(-5, 5, length=n)
+    dx = x[2] - x[1]
     
     # add noise to data
     rng = MersenneTwister(rng_seed)
-    data = f.(x) + (rand(rng, n) .- 0.5) * 0.05
-
+    data = f.(x) + 0.05 * (rand(rng, n) .- 0.5)
+    
     # use TVRegDiff
-    û = TVRegDiff(data, iter, 0.2, scale="large", 
-            preconditioner=preconditioner,   
-            plot_flag=true, diag_flag=false)
+    û = TVRegDiff(data, iter, 0.1, dx=dx, scale="large", 
+            plot_flag=false, diag_flag=false,
+            preconditioner=preconditioner) 
 
     return rmse = √(mean(abs2.(û - u.(x))))
 end
@@ -25,23 +29,25 @@ end
     û = TVRegDiff(data, 1, 0.2, scale="large",  
             plot_flag=false, diag_flag=false)
 
-    @test length(û) == n # output dim equals input dim
+    @test length(û) == n # output dim should equal input dim
 end
 
-@testset "symbolic functions" begin
-    @testset "abs" begin
-        @test _eval_noisy(abs, sign, "amg_rs") < 0.3
-        @test _eval_noisy(abs, sign, "amg_rs", iter=10) < 0.2
-    end
-    @testset "sigmoid" begin
-        σ(x) = exp(x)/(1+exp(x))
-        dσdx(x) = σ(x) - exp(2*x)/(1+exp(x)^2)
+for pc in ["cholesky","diagonal","amg_rs","amg_sa"]
+    @testset "$pc" begin
+        @testset "abs" begin
+            @test _eval_large_noisy(abs, sign, preconditioner=pc, iter=1) < 0.3
+            @test _eval_large_noisy(abs, sign, preconditioner=pc, iter=10) < 0.2
+        end
+        @testset "sigmoid" begin
+            σ(x) = exp(x) / (1 + exp(x))
+            dσdx(x) = σ(x) - exp(2 * x) / (1 + exp(x)^2)
 
-        @test _eval_noisy(σ, dσdx, "amg_rs") < 0.3
-        @test _eval_noisy(σ, dσdx, "amg_rs", iter=10) < 0.2    
+            @test _eval_large_noisy(σ, dσdx, preconditioner=pc, iter=1) < 0.3
+            @test _eval_large_noisy(σ, dσdx, preconditioner=pc, iter=10) < 0.2   
+        end
+        @testset "sin" begin
+            @test _eval_large_noisy(sin, cos, preconditioner=pc, iter=1) < 0.3
+            @test _eval_large_noisy(sin, cos, preconditioner=pc, iter=10) < 0.2
+        end     
     end
-    @testset "sin" begin
-        @test _eval_noisy(sin, cos, "amg_rs") < 0.3
-        @test _eval_noisy(sin, cos, "amg_rs", iter=10) < 0.2
-    end     
 end
--- test/TVRegDiff_small_test.jl
+++ test/TVRegDiff_small_test.jl
@@ -3,17 +3,19 @@ using Random
 using Statistics
 using Test
 
-function _eval_noisy(f, u; iter=1, rng_seed=0)
+function _eval_small_noisy(f, u ; iter=1, precond_flag=false, rng_seed=0)
     n = 50 
     x = range(-5, 5, length=n)
+    dx = x[2] - x[1]
     
     # add noise to data
     rng = MersenneTwister(rng_seed)
     data = f.(x) + (rand(rng, n) .- 0.5) * 0.05
 
     # use TVRegDiff
-    û = TVRegDiff(data, iter, 0.2, scale="small",  
-            plot_flag=true, diag_flag=false)
+    û = TVRegDiff(data, iter, 0.2, dx=dx, scale="small",  
+            plot_flag=false, diag_flag=false,
+            precond_flag=precond_flag)
 
     return rmse = √(mean(abs2.(û - u.(x))))
 end
@@ -24,23 +26,25 @@ end
     û = TVRegDiff(data, 1, 0.2, scale="small",  
             plot_flag=false, diag_flag=false)
 
-    @test length(û) == n # output dim equals input dim
+    @test length(û) == n # output should equal input dim
 end
 
-@testset "symbolic functions" begin
-    @testset "abs" begin
-        @test _eval_noisy(abs, sign) < 0.3
-        @test _eval_noisy(abs, sign, iter=10) < 0.2
-    end
-    @testset "sigmoid" begin
-        σ(x) = exp(x)/(1+exp(x))
-        dσdx(x) = σ(x) - exp(2*x)/(1+exp(x)^2)
+for pf in [true, false]
+    @testset "precondflag=$(pf)" begin
+        @testset "abs" begin
+            @test _eval_small_noisy(abs, sign, precond_flag=pf, iter=1) < 0.3
+            @test _eval_small_noisy(abs, sign, precond_flag=pf, iter=10) < 0.2
+        end
+        @testset "sigmoid" begin
+            σ(x) = exp(x) / (1 + exp(x))
+            dσdx(x) = σ(x) - exp(2 * x) / (1 + exp(x)^2)
 
-        @test _eval_noisy(σ, dσdx) < 0.3
-        @test _eval_noisy(σ, dσdx, iter=10) < 0.2    
-    end
-    @testset "sin" begin
-        @test _eval_noisy(sin, cos) < 0.3
-        @test _eval_noisy(sin, cos, iter=10) < 0.2
+            @test _eval_small_noisy(σ, dσdx, precond_flag=pf, iter=1) < 0.3
+            @test _eval_small_noisy(σ, dσdx, precond_flag=pf, iter=10) < 0.2  
+        end
+        @testset "sin" begin
+            @test _eval_small_noisy(sin, cos, precond_flag=pf, iter=1) < 0.3
+            @test _eval_small_noisy(sin, cos, precond_flag=pf, iter=10) < 0.2
+        end
     end
 end
