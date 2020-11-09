6df5d8820cabff890746bccca29ce785ad34cfe6
Adrian Hill <adrian.hill@mailbox.org>
2020-11-09 17:45:56 +0100

    Tighten bounds for test on "demo_large"

--- test/demo_large_test.jl
+++ test/demo_large_test.jl
@@ -4,7 +4,7 @@ function _testset_demo_large(data_demo_large, scale, precond)
     Use parameter-sets from MATLAB examples. =#
 
     @testset "$precond" begin
-        @test sum(abs.(diff(TVRegDiff(data_demo_large, 40, 1e-1, ε=1e-8, scale=scale, precond=precond)))) < 1e4  
-        @test sum(abs.(diff(TVRegDiff(data_demo_large, 40, 1e-3, ε=1e-6, scale=scale, precond=precond)))) < 1e4  
+        @test sum(abs.(diff(TVRegDiff(data_demo_large, 40, 1e-1, ε=1e-8, scale=scale, precond=precond)))) < 1e3
+        @test sum(abs.(diff(TVRegDiff(data_demo_large, 40, 1e-3, ε=1e-6, scale=scale, precond=precond)))) < 1e3  
     end
 end
