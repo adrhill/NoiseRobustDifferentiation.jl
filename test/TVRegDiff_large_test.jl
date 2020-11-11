04917af48a7b1a1650dc4fbe217ef813803f65bf
Adrian Hill <adrian.hill@mailbox.org>
2020-11-11 16:44:41 +0100

    Remove test with precond `diagonal`
    Shorten runtime of tests. Keep `amg_rs` which is run in the docs.

--- test/TVRegDiff_large_test.jl
+++ test/TVRegDiff_large_test.jl
@@ -10,7 +10,7 @@ include("demo_large_test.jl")
 
 _testset_output_dim("large")
 _testset_symbolic_functions("large", ["amg_rs","amg_sa","cholesky","diagonal","none"], ["abs","square"])
-_testset_demo_large("large", ["amg_rs","diagonal"], ["abs"])
+_testset_demo_large("large", ["amg_rs"], ["abs"])
 
 @testset "Broken inputs" begin
     @test_throws ArgumentError TVRegDiff([0, 1, 2], 1, 0.1, scale="large", precond="bad_input")
