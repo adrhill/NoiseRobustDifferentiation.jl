fcb3484027f27a8ceff9cd305e2d865565357881
Documenter.jl <documenter@juliadocs.github.io>
2020-11-12 12:06:06 +0000

    build based on cddb773

--- dev/plot_example.jl
+++
@@ -1,9 +0,0 @@
-using Plots
-using LaTeXStrings
-
-function plot_example(x, f, data, u, u_diff, û)
-    pf = plot(x, [f, data], c=[:grey :2], ylabel=L"f", label=[L"f" L"f_{noisy}"])
-    pu = plot(x, [u,u_diff,û], c=[:grey :2 :1], ylabel=L"u", label=[L"u" L"\hat{u}_{diff}" L"\hat{u}_{TVRegDiff}"])
-    
-    plot(pf, pu, layout=(2, 1))
-end
