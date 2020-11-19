468b8a0b93ab07a49bc38eb26538d56b99dec54a
Adrian Hill <adrian.hill@mailbox.org>
2020-11-19 14:25:35 +0100

    Fix testing for parameters `diff_kernel`
    Some heuristically set thresholds for testing were relaxed where it seemed reasonable

--- test/TVRegDiff_large_test.jl
+++ test/TVRegDiff_large_test.jl
@@ -14,6 +14,7 @@ _testset_demo_large("large", ["amg_rs"], ["abs"])
 
 @testset "Broken inputs" begin
     @test_throws ArgumentError TVRegDiff([0, 1, 2], 1, 0.1, scale="large", precond="bad_input")
+    @test_throws ArgumentError TVRegDiff([0, 1, 2], 1, 0.1, scale="large", diff_kernel="bad_input")
     @test_throws ArgumentError TVRegDiff([0, 1, 2], 1, 0.1, scale="l4rge")
     @test_throws DimensionMismatch TVRegDiff([0, 1, 2], 1, 0.1, u_0=[1,2,3,4], scale="large")
 end
